071b1910f47209205a8cd5ba3fc7e1ba6f1ed1f8
magdamiu <badita.magda@gmail.com>
2020-04-28 21:27:50 +0300

    Week3

--- app/src/main/java/com/magdamiu/androidfundamentals04/LoginActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentals04/LoginActivity.java
@@ -3,12 +3,54 @@ package com.magdamiu.androidfundamentals04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailAddressEditText;
+    private EditText phoneEditText;
+    private CheckBox terms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailAddressEditText = findViewById(R.id.emailAddressEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
+        terms = findViewById(R.id.termsCheckBox);
+
+        emailAddressEditText.setText("android@gmail.com");
+    }
+
+    public void loginOnClick(View view) {
+        String email = emailAddressEditText.getText().toString();
+        String phone = phoneEditText.getText().toString();
+        if (email.isEmpty()) {
+            emailAddressEditText.setError(getString(R.string.error_validate_email));
+        }
+        if (phone.isEmpty()) {
+            phoneEditText.setError("Please fill the phone number");
+        }
+        if (terms.isChecked() == false) {
+            Toast.makeText(this, "Please accept terms and conditions", Toast.LENGTH_LONG).show();
+        }
+
+        if (!email.isEmpty() && !phone.isEmpty() && terms.isChecked()) {
+            Toast.makeText(this, email + " " + phone, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/magdamiu/androidfundamentals04/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentals04/MainActivity.java
@@ -3,12 +3,54 @@ package com.magdamiu.androidfundamentals04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String ANDROID_URL = "https://developer.android.com/";
+
+    private WebView webView;
+    private Spinner spinner;
+
+    private List<String> fructe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /* initial setup */
+//        setContentView(R.layout.activity_main);
+
+        /* webview sample */
+//        setContentView(R.layout.webview_sample);
+//
+//        webView = findViewById(R.id.webViewAndroid);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl(ANDROID_URL);
+
+        setContentView(R.layout.spinner_sample);
+        spinner = findViewById(R.id.spinnerFructe);
+        setSpinnerSource();
+        spinner.setAdapter(getSpinnerAdapter());
     }
+
+    private void setSpinnerSource(){
+        fructe = new ArrayList<>();
+        fructe.add("Mar");
+        fructe.add("Para");
+        fructe.add("Mango");
+        fructe.add("Cirese");
+        fructe.add("Fructul pasiunii");
+        fructe.add("Zmeura");
+        fructe.add("Rodie");
+        fructe.add("Rosie");
+    }
+
+    private ArrayAdapter<String> getSpinnerAdapter() {
+        return new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, fructe);
+    }
+
 }
