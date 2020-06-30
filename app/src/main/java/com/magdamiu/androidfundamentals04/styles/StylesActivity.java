53fa821a2062a2aff44ca769800a59d81ea8c5ef
magdamiu <badita.magda@gmail.com>
2020-06-30 20:38:30 +0300

    animation sample plus asking for camera permission

--- app/src/main/java/com/magdamiu/androidfundamentals04/styles/StylesActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentals04/styles/StylesActivity.java
@@ -1,9 +1,14 @@
 package com.magdamiu.androidfundamentals04.styles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,12 +17,36 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 import com.magdamiu.androidfundamentals04.R;
 
+import static android.Manifest.permission.CAMERA;
+
 public class StylesActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_CAMERA = 23;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_styles);
+
+        constraintLayout = findViewById(R.id.constraintParent);
+        setAnimation();
+
+        if (ContextCompat.checkSelfPermission(StylesActivity.this, CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // unhappy path
+            ActivityCompat.requestPermissions(StylesActivity.this, new String[]{CAMERA}, REQUEST_CODE_CAMERA);
+        } else {
+            // happy path
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // user accepted to grant camera permission
+            }
+        }
     }
 
     public void fabOnClick(View view) {
@@ -35,5 +64,12 @@ public class StylesActivity extends AppCompatActivity {
         TextView textView = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text);
         textView.setTextColor(Color.GREEN);
         snackbar.show();
+    }
+
+    private void setAnimation() {
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
     }
 }
